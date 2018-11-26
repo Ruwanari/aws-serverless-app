fec17fbadae075bb373d515fa61245f345faf544
Ruwanari <ruwanari.h@eyepax.com>
2018-11-26 13:48:51 +0530

    code changed to former version

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,8 +1,7 @@
-message = {
-   'message': 'Execution started successfully!'
-}
-return {
-    'statusCode': 200,
-    'headers': {'Content-Type': 'application/json'},
-    'body': json.dumps(message)
-}
+import boto3
+import json
+
+ec2 = boto3.client('ec2')
+def lambda_handler(event, context):
+    response = ec2.describe_availability_zones()
+    return {"statusCode": 200, "body": json.dumps(response)}
